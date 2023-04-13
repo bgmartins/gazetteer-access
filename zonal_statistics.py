4648b195c626a22847fb8c9c15eb21799ede6a5c
bgmartins <bgmartins@gmail.com>
2023-04-13 08:41:11 +0100

    Minor changes.

--- zonal_statistics.py
+++ zonal_statistics.py
@@ -5,11 +5,18 @@ import bz2
 import rasterio
 import rasterstats
 
-dem = rasterio.open(dem_fp)
-polygon = kallio.to_crs(crs=dem.crs.data)
+def zonal_statistics( name , raster, stats=['min', 'max', 'mean', 'median', 'majority', 'minority', 'unique', 'count', 'sum', 'std']):
+    raster_data = rasterio.open(raster)
+    polygon = geocode.geocode_placename(name)
+    polygon = polygon.to_crs(crs=dem.crs.data)
+    array = raster_data.read(1)
+    affine = raster_data.affine
+    polygon = rasterstats.zonal_stats(polygon, array, affine=affine, stats=stats, geojson_out=True)
+    return polygon
 
-array = dem.read(1)
-affine = dem.affine
-kallio = rasterstats.zonal_stats(polygon, array, affine=affine, stats=['min', 'max', 'mean', 'median', 'majority', 'minority', 'unique', 'count', 'sum', 'std'], geojson_out=True)
+if __name__ == "__main__":
+    aux = geocode_placename("Lisbon", "gpw_v4_population_count_rev11_2020_30_sec.tif")
+    GeoJSONFeatureCollectionAsString = geojson.dumps(aux)
+    print(GeoJSONFeatureCollectionAsString)
 
 
