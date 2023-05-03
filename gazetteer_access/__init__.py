a9cc4a195dbd730ce17f10a17164d40e0ed350d6
Bruno Martins <bgmartins@users.noreply.github.com>
2023-05-03 06:18:10 +0100

    Minor changes

--- gazetteer_access/__init__.py
+++ gazetteer_access/__init__.py
@@ -3,9 +3,7 @@ gazetteer_access.
 
 Simple Python library supporting the access to the who's on first gazetteer data.
 """
-from .geocode import geocode_placename
-from .reverse_geocode import reverse_geocode_point
-from .zonal_statistics import zonal_statistics
+from .gazetteer_access import geocode_placename, reverse_geocode_point, zonal_statistics
 
 __version__ = "0.1.0"
 __author__ = 'Bruno Martins'
---
+++ gazetteer_access/gazetteer_access.py
@@ -0,0 +1,3 @@
+from .geocode import geocode_placename
+from .reverse_geocode import reverse_geocode_point
+from .zonal_statistics import zonal_statistics
