43d49b96497615b36be964ddb111e0f173ffdea7
Bruno Martins <bgmartins@users.noreply.github.com>
2023-05-03 09:26:55 +0100

    Minor changes.

--- setup.py
+++ setup.py
@@ -3,6 +3,10 @@ import sqlite3
 import urllib
 import gzip
 import bz2
+import logging
+import sys
+import os
+import tempfile
 from zipfile import ZipFile
 from setuptools import setup
 from setuptools.command.develop import develop
@@ -10,6 +14,9 @@ from setuptools.command.install import install
 
 wof_data = "https://data.geocode.earth/wof/dist/spatial/whosonfirst-data-admin-latest.spatial.db.bz2"
 pop_data = "https://sedac.ciesin.columbia.edu/downloads/data/gpw-v4/gpw-v4-population-count-rev11/gpw-v4-population-count-rev11_2020_30_sec_tif.zip"
+
+logging.basicConfig(stream=sys.stderr, level=logging.INFO)
+log = logging.getLogger()
 
 def read_file_from_uri(uri):
     if uri.startswith('http'):
@@ -26,15 +33,21 @@ def download_file(url, filename):
     with open(filename, 'wb') as f:
         f.write(read_file_from_uri(url).read())
 
-#def create_population(username, password, filename):
-#    filename = os.path.basename(urlparse(pop_data).path)
-#    r = requests.get(url, auth=(username,password))
-#    with ZipFile(r, 'r') as zipObj:
-#        zipObj.extract("", path=filename, pwd=None)
+def create_population(filename):
+    log.info("Downloading data from https://sedac.ciesin.columbia.edu")
+    aux = urllib.parse.urlparse(pop_data)
+    file = urllib.request.urlopen(pop_data)
+    temp = tempfile.NamedTemporaryFile()
+    temp.write(file.read())
+    temp.close()
+    with ZipFile(temp.name, 'r') as zipObj:
+        aux = urllib.parse.urlparse(pop_data)
+        extract_file = os.path.basename(aux.path).replace(".zip","")
+        zipObj.extract(extract_file, path=filename, pwd=None)
 
 def create_database(filename):
-    print("Downloading data from https://geocode.earth/data/whosonfirst/combined/", file=sys.stderr)
-    #download_file(wof_data, filename)
+    log.info("Downloading data from https://geocode.earth/data/whosonfirst/combined/")
+    download_file(wof_data, filename)
     conn = sqlite3.connect(filename)
     conn.commit()
     conn.execute("vacuum")
@@ -44,15 +57,19 @@ class PostDevelopCommand(develop):
     """Post-installation for development mode."""
     def run(self):
         develop.run(self)
-        db = pkg_resources.resource_filename(__name__, 'gazetteer.db')
-        create_database(db)
+        #po = pkg_resources.resource_filename(__name__, 'population.tif')
+        #create_population(po)
+        #db = pkg_resources.resource_filename(__name__, 'gazetteer.db')
+        #create_database(db)
 
 class PostInstallCommand(install):
     """Post-installation for installation mode."""
     def run(self):
         install.run(self)
-        db = pkg_resources.resource_filename(__name__, 'gazetteer.db')
-        create_database(db)        
+        #po = pkg_resources.resource_filename(__name__, 'population.tif')
+        #create_population(po)
+        #db = pkg_resources.resource_filename(__name__, 'gazetteer.db')
+        #create_database(db)        
 
 setup(
     name='gazetteer_access',
