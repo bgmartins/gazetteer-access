2a6597f46197fd602be505d57ccf767aafbcd6ac
Bruno Martins <bgmartins@users.noreply.github.com>
2023-05-03 16:29:10 +0100

    Minor changes

--- gazetteer_access/reverse_geocode.py
+++ gazetteer_access/reverse_geocode.py
@@ -5,13 +5,14 @@ import fiona
 import pyproj
 from shapely import geometry
 
-def reverse_geocode_point( lat, lon, EPSG=32632, onlyFirst = True, vectorNet = True, geoJSON = True ):
-    if vectorNet:
-        shapefile = pkg_resources.resource_filename(__name__, 'vectornet_polygons/VectornetDATAforMOOD.shp')
-        shapes = fiona.open(shapefile)
+_vectornet_shapefile = pkg_resources.resource_filename(__name__, 'vectornet_polygons/VectornetDATAforMOOD.shp')
+_vectornet_shapes = fiona.open(_vectornet_shapefile)
+
+def reverse_geocode_point( lat, lon, EPSG=32632, onlyFirst = True, vectornet = True, geoJSON = True ):
+    if vectornet:
         featureCollection = list()
         p = geometry.Point(lon, lat)
-        for region in shapes:
+        for region in _vectornet_shapes:
             s = geometry.shape(region["geometry"])
             if s.contains(p): 
                 featureCollection.append(region)
@@ -32,10 +33,8 @@ def reverse_geocode_point( lat, lon, EPSG=32632, onlyFirst = True, vectorNet = T
     if geoJSON: featureCollection = geojson.dumps(featureCollection)
     return featureCollection
 
-def reverse_geocode_geometry( polygonal_region , EPSG=32632, onlyFirst = True, vectorNet = True, geoJSON = True ):
-    if vectorNet:
-        shapefile = pkg_resources.resource_filename(__name__, 'vectornet_polygons/VectornetDATAforMOOD.shp')
-        shapes = fiona.open(shapefile)
+def reverse_geocode_geometry( polygonal_region , EPSG=32632, onlyFirst = True, vectornet = True, geoJSON = True ):
+    if vectornet:
         featureCollection = list()
         if isinstance(polygonal_region, str):
             try: 
@@ -44,7 +43,7 @@ def reverse_geocode_geometry( polygonal_region , EPSG=32632, onlyFirst = True, v
                 p = geometry.shape(geojson.loads(polygonal_region))
         else: p = geometry.shape(polygonal_region)
         max_area = 0
-        for region in shapes:
+        for region in _vectornet_shapes:
             s = geometry.shape(region["geometry"])
             area = s.intersection(p).area / p.area
             if area > max_area: 
@@ -67,12 +66,16 @@ def reverse_geocode_geometry( polygonal_region , EPSG=32632, onlyFirst = True, v
     return featureCollection
 
 if __name__ == "__main__":
-    aux = reverse_geocode_point(38.7223, -9.1393)
-    print(aux)
+    print("Test the reverse geocoding of a point corresponding to Lisbon, Portugal...")
+    result = reverse_geocode_point(38.7223, -9.1393)
+    print(result)
+    print()
 
-    lisbon = {
+    print("Test the reverse geocoding of a geometry that approximates the boundary of Lisbon, Portugal...")
+    geom = {
         "type": "Polygon",
         "coordinates": [ [ [-9.2242, 38.6916], [-9.2394, 38.7267], [-9.1683, 38.7397], [-9.1355, 38.7209], [-9.1417, 38.6974], [-9.2242, 38.6916] ] ]
     }
-    aux = reverse_geocode_geometry(lisbon)
-    print(aux)
+    result = reverse_geocode_geometry(geom)
+    print(result)
+    print()
