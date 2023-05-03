3922c92337b5c4582940a9b7048b25b5209ca590
Bruno Martins <bgmartins@users.noreply.github.com>
2023-05-03 05:38:32 +0100

    Minor changes in project organization.

---
+++ gazetteer-access/__init__.py
@@ -0,0 +1,9 @@
+"""
+gazetteer-access.
+
+Simple Python library supporting the access to the who's on first gazetteer data.
+"""
+
+__version__ = "0.1.0"
+__author__ = 'Bruno Martins'
+__credits__ = 'MOOD Project'
---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='gazetteer-access',
+    version='0.1.0',    
+    description="Simple Python library supporting the access to the who's on first gazetteer data.",
+    url='https://github.com/bgmartins/gazetteer-access',
+    author='Bruno Martins',
+    author_email='bgmartins@gmail.com',
+    license='BSD 2-clause',
+    packages=['gazetteer-access'],
+    install_requires=['rasterio', 'rasterstats', 'spatialite', 'geojson', ],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',  
+        'Operating System :: POSIX :: Linux',        
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
